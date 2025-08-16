e5608eeb9aef5c2a57d5d14b7ca8f081fa50c467
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-16 22:04:02 +0200

    delete wrong files

--- css.php
+++
@@ -1,8 +0,0 @@
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/accordion.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/content.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/footer.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/main.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/navigation.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/now.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/sidebar.css">
-<link rel="stylesheet" href="https://codevoyage.samwilliam.de/2.0.0/styles/ortsliste.css">
