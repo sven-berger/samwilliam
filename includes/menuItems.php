2748b547e6b337fd7ce4c296ed0868e49508816b
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-17 04:49:24 +0200

    add target _blank

--- includes/menuItems.php
+++ includes/menuItems.php
@@ -3,7 +3,7 @@
 $navLinks = [
   ['href' => 'index.php?page=index', 'label' => 'Startseite'],
   ['href' => 'index.php?page=zahlen-raten', 'label' => 'Zahlen raten'],
-  ['href' => '/web-kompakt/', 'label' => 'Web kompakt'],
+  ['href' => '/web-kompakt/', 'label' => 'Web kompakt', 'target' => '_blank'],
   ['href' => '/index.php?page=mein-wandel', 'label' => 'Mein Wandel']
 ];
 
