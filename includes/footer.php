9a2d00e04058bcec408b39ed0ef3cf921c52d356
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-18 13:40:56 +0200

    KISS-Prinzip
    Signed-off-by: Sven Oliver Berger <bergersvenoliver@gmail.com>

--- includes/footer.php
+++ includes/footer.php
@@ -1,25 +1,28 @@
   </main>
 </div>
 
+<!-- Font Awesome 6 Free einbinden -->
+<link rel="stylesheet" href="/assets/fontawesome/css/all.min.css">
+
 <!-- HightLight.js einbinden -->
-<link rel="stylesheet" href="https://samwilliam.de/assets/highlightjs/styles/default.min.css">
-<script src="https://samwilliam.de/assets/highlightjs/highlight.min.js"></script>
+<link rel="stylesheet" href="/assets/highlightjs/styles/default.min.css">
+<script src="/assets/highlightjs/highlight.min.js"></script>
 <script>hljs.highlightAll();</script>
 
 <!-- TinyMCE-Editor einbinden -->
-<script src="https://samwilliam.de/assets/tinymce/tinymce.min.js"></script>
+<script src="/assets/tinymce/tinymce.min.js"></script>
 <script>
 tinymce.init({
     selector: 'textarea',
     license_key: 'gpl',
     content_css:
     [
-        'https://samwilliam.de/assets/highlightjs/styles/default.min.css',
-        'https://samwilliam.de/stylesheet/tm-editor.css'
+        '/assets/highlightjs/styles/default.min.css',
+        '/stylesheet/tm-editor.css'
     ],
     menubar: false,
     language: 'de',
-    language_url: 'https://samwilliam.de/assets/tinymce/langs/de.js',
+    language_url: '/assets/tinymce/langs/de.js',
     plugins: 'code table lists fullscreen wordcount link image autosave advlist codesample preview',
     toolbar: 'code undo redo | bold italic | blocks | link image codesample table blockquote | bullist numlist | alignleft aligncenter alignright removeformat preview',
     fontsize_formats: "10pt 12pt 14pt 16pt 18pt 24pt 36pt"
