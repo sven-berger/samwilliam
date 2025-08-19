95f2c621c5beefe3a94ffbf2614f748875767d5b
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-19 13:26:56 +0200

    add content

--- libary/index.lib.php
+++ libary/index.lib.php
@@ -1,11 +1,15 @@
 <h1 class="text-7xl font-bold mb-10">
   Startseite
 </h1>
-<section class="mb-20">
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<section class="mb-20 content">
+  <p>Willkommen auf meiner neuen Seite!</p>
+  <p>Nach langer Vorbereitung ist sie endlich online – modern, schlank und ganz nach meinen Vorstellungen aufgebaut.</p><br>
+  <p>Statt auf das bekannte <a href="https://getbootstrap.com/">Bootstrap</a> setze ich hier auf <a href="https://tailwindcss.com/">Tailwind CSS</a>. So bleibt die Seite von Anfang an responsiv, übersichtlich und frei von unnötigem Ballast.</p>
+  <p>Im Backend läuft aktuell noch <a href="https://php.net">PHP</a>, aber mittelfristig möchte ich auf <a href="https://rubyonrails.org/">Ruby on Rails</a> umsteigen.</p>
+  <p>Im Frontend vertraue ich auf <a href="https://stimulus.hotwired.dev/">JavaScript mit Stimulus</a>, das für eine klare Trennung von HTML und JavaScript sorgt – genau die Struktur, die ich mir immer gewünscht habe.</p><br>
+  <p>Mein Ziel war es, eine Seite zu schaffen, die schlicht, funktional und zukunftsorientiert ist. Nur drei Farben, ein schlankes Stylesheet und minimale Zusatz-CSS sorgen dafür, dass alles aufgeräumt bleibt.<p>
+  <p>Diese Seite ist mein persönliches Projekt, mit dem ich nicht nur neue Dinge ausprobiere, sondern auch kontinuierlich dazulerne.</p>
+  <p>Und während bis zu meiner Abschlussprüfung im September 2025 das Lernen Vorrang hat, freue ich mich schon jetzt auf alles, was noch kommt. 🚀</p>
 </section>
 
 <section class="mb-40">
