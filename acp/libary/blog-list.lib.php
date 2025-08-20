05766bb1641812ac503dc1c33fa65a56e254de98
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-20 21:54:12 +0200

    Structure
    Signed-off-by: Sven Oliver Berger <bergersvenoliver@gmail.com>

--- acp/libary/blog-list.lib.php
+++ acp/libary/blog-list.lib.php
@@ -9,25 +9,35 @@
   $artikel = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
-<table>
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Titel</th>
-      <th>Datum</th>
-      <th>Aktion</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php foreach ($artikel as $eintrag): ?>
-    <tr>
-      <td><?= htmlspecialchars($eintrag['id']) ?></td>
-      <td><?= htmlspecialchars($eintrag['headline']) ?></td>
-      <td><?= date('d.m.Y', strtotime($eintrag['created_at'])) ?>, <?= date('H:i', strtotime($eintrag['created_at'])) ?> Uhr</td>
-      <td>
-        <a href="index.php?page=article-edit&id=<?= urlencode($eintrag['id']) ?>" class="text-blue-500 hover:underline">Bearbeiten</a>
-      </td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+<section class="mb-10 flex justify-end">
+  <a href="/acp/index.php?page=article-add" class="px-5 py-3 bg-red-500 text-white font-semibold rounded-md hover:opacity-90">
+    Artikel verfassen
+  </a>
+</section>
+
+<section>
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Titel</th>
+        <th>Datum</th>
+        <th>Aktion</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach ($artikel as $eintrag): ?>
+      <tr>
+        <td><?= htmlspecialchars($eintrag['id']) ?></td>
+        <td><?= htmlspecialchars($eintrag['headline']) ?></td>
+        <td><?= date('d.m.Y', strtotime($eintrag['created_at'])) ?>, <?= date('H:i', strtotime($eintrag['created_at'])) ?> Uhr</td>
+        <td>
+          <a href="index.php?page=article-edit&id=<?= urlencode($eintrag['id']) ?>" class="text-blue-500 hover:underline">Bearbeiten</a>
+        </td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</section>
+
+
