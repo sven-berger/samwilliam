196eb305b85bb2838e00ca1f5ee69316d3b81cfd
Sven Oliver Berger <bergersvenoliver@gmail.com>
2025-08-21 09:00:31 +0200

    finish index

--- libary/index.lib.php
+++ libary/index.lib.php
@@ -10,7 +10,8 @@
 <section class="mb-10">
   <h2 class="inline-block text-5xl border-b-4 text-red-500 border-orange-400 pb-2 mb-10 font-bold">Aktueller Wissensstand</h2>
   <div class="flex flex-col lg:flex-row gap-6">
-    <div class="flex-1 bg-white border border-gray-300 rounded-lg p-6 hover:shadow-lg hover:border-red-500 hover:border-4 hover:bg-white transition duration-300">  
+    <div class="flex-1 bg-white border border-gray-300 rounded-lg p-6 hover:shadow-lg hover:border-red-500 hover:border-4 hover:bg-white transition duration-300">
+      <a href="#IHK1">
       <div class="flex justify-between text-xl font-semibold text-red-500 mb-2">
         <span class="flex items-center">
           IHK-Abschlussprüfung I
@@ -21,7 +22,9 @@
       </div>   
       <p class="text-gray-600">Die IHK-Abschlussprüfung Teil 1 ist die erste große Zwischenprüfung für angehende Fachinformatiker für Anwendungsentwicklung.<br>Hier werden vor allem die praktischen Grundlagen geprüft, die man in während der Ausbildung/Umschulung gelernt hat.</p>
     </div>
+    </a>
     <div class="flex-1 bg-white border border-gray-300 rounded-lg p-6 hover:shadow-lg hover:border-red-500 hover:border-4 hover:bg-white transition duration-300">
+      <a href="#StimulusTailwind">
       <div class="flex justify-between text-xl font-semibold text-red-500 mb-2">
         <span class="flex items-center">
           Stimulus & Tailwind
@@ -32,8 +35,10 @@
       </div>
       <p class="text-gray-600">Stimulus ist ein JavaScript-Framework, das dafür sorgt, dass HTML und JavaScript klar getrennt bleiben.<br>Tailwind ist ein CSS-Framework, mit dem sich moderne Webseiten schnell gestalten lassen.</p>
     </div>
+    </a>
     <div class="flex-1 bg-white border border-gray-300 rounded-lg p-6 hover:shadow-lg hover:border-red-500 hover:border-4 hover:bg-white transition duration-300">
-       <div class="flex justify-between text-xl font-semibold text-red-500 mb-2">
+    <a href="#PHP">   
+    <div class="flex justify-between text-xl font-semibold text-red-500 mb-2">
         <span class="flex items-center">
           PHP
         </span>
@@ -43,15 +48,42 @@
       </div>
       <p class="text-gray-600">PHP ist eine weit verbreitete Programmiersprache, die vor allem für Webseiten und Webanwendungen genutzt wird.<br>Damit lassen sich Inhalte dynamisch erzeugen und mit Datenbanken verbinden.</p>
     </div>
+    </a>
   </div>
 </section>
 
-<section class="mb-10">
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<section class="mb-10 content" id="IHK1">
+  <h3>Meine Vorbereitung auf die IHK-Abschlussprüfung Teil 1</h3>
+  <p>In den letzten Monaten habe ich mich intensiv auf die IHK-Abschlussprüfung Teil 1 vorbereitet. Dabei habe ich nicht nur die theoretischen Grundlagen gelernt, sondern auch praktische Erfahrungen
+gesammelt. Ich habe an verschiedenen Projekten gearbeitet, um mein Wissen in der Praxis anzuwenden und zu vertiefen.</p>
+  <p>Die wichtigsten Themen, die ich gelernt habe, sind:</p>
+  <ul>
+    <li>Hardware und Software</li>
+    <li>Netzwerktechnologien</li>
+    <li>Programmierung mit Python</li>
+    <li>Programmierung mit Java</li>
+    <li>Rechtliche Aspekte der IT</li>
+    <li>Projektmanagement</li>
+    <li>IT-Sicherheit und Datenschutz</li>
+  </ul>
+  <p>Auch habe ich mich privat immer weiter mit der Webentwicklung beschäftigt, um meine Kenntnisse zu erweitern und neue Technologien kennenzulernen.</p>
 </section>
+
+<section class="mb-10 content" id="StimulusTailwind">
+  <h3>Meine erste Erfahrung mit Stimulus & Tailwind</h3>
+  <p>In den letzten Wochen habe ich mich intensiv mit Stimulus und Tailwind beschäftigt. Diese beiden Technologien haben mir geholfen, moderne und ansprechende Webanwendungen zu entwickeln.</p>
+  <p>Ich finde es spannend, wie Stimulus es ermöglicht, JavaScript und HTML klar und strukturiert von einander zu trennen. Dadurch bleibt der Code übersichtlich und wartbar.</p>
+  <p>Tailwind hingegen hat mir gezeigt, wie es Dank dieses Werkzeuges es möglich ist, ein konsistentes Design zu erstellen, ohne viel Zeit mit dem Schreiben von CSS zu verbringen.</p>
+</section>
+
+<section class="mb-40 content" id="PHP">
+  <h3>Meine Leidenschaft für PHP</h3>
+  <p>PHP ist seit meiner Jugend eine große Leidenschaft in der Programmierung von mir – wahrscheinlich, weil es die erste Sprache war, die ich kennengelernt habe.<7p>
+  <p>Bis heute begeistert sie mich, da PHP in der Regel ohne Framework auf nahezu jedem Server verfügbar ist und sich ein lokaler Webserver damit unkompliziert aufsetzen lässt.</p>
+  <p>Zu meinen Kenntnissen gehören die Grundlagen wie Funktionen, Arrays, Schleifen und die Anbindung an MySQL, aber auch objektorientierte Programmierung.</p><br>
+  <p>Und auch wenn viele über PHP lästern und behaupten, es sei eine „tote Sprache“ – für mich bleibt sie meine Lieblingssprache, die ich neben JavaScript und Ruby (on Rails) auch in Zukunft weiter nutzen möchte.</p>
+</section>
+
 
 <section class="mb-10">
   <h2 class="inline-block text-5xl mt-5 mb-10 border-b-4 text-red-500 border-orange-400 pb-2 font-bold">Zukunftspläne</h2>
@@ -92,9 +124,23 @@
   </div>
 </section>
 
-<section>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-  <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
-  <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+<section class="mb-10 content" id="Rails">
+  <h3>Rails, die Alternative zu PHP?</h3>
+  <p>Ich werde Ruby on Rails lernen, weil es die oben genannten Vorteile bietet, viele Möglichkeiten von Haus aus mit sich bringt und dadurch den Programmieraufwand deutlich reduziert.</p>
+  <p>Zusätzlich ist Rails für mich interessant, da mein Praktikumsbetrieb von PHP auf Rails gewechselt ist und ich dadurch mit der Arbeitsweise direkt vertraut werden kann.</p>
+  <p>Ein weiterer Pluspunkt: Mit Stimulus, das von den gleichen Entwicklern stammt, sowie der guten Zusammenarbeit mit React ergeben sich spannende Möglichkeiten für zukünftige Projekte.</p>
+</section>
+
+<section class="mb-10 content" id="React">
+  <h3>Das Webmonster React</h3>
+  <p>Ich möchte React erlernen, weil es im modernen Web nahezu allgegenwärtig ist – egal ob Webseiten oder Webapps.</p>
+  <p>Als JavaScript-Bibliothek bietet es mächtige Funktionen und erlaubt sogar die Entwicklung von Anwendungen für Smart-Geräte.</p>
+  <p>Besonders interessant finde ich, wie nahtlos React mit Backends wie Rails oder PHP zusammenarbeitet.</p>
+  <p>Auch mein Praktikumsbetrieb setzt auf React: Für kleinere Projekte wird dort Stimulus genutzt, bei größeren Projekten hingegen React – und genau diesen Praxisbezug möchte ich für mich aufbauen.</p>
+</section>
+<section class="mb-10 content" id="DatenbankArchitektur">
+  <h3>Datenbank-Architektur verstehen</h3>
+  <p>Ein weiterer Bereich, den ich vertiefen möchte, viel mehr muss, ist die Datenbank-Architektur.</p>
+  <p>Mich fasziniert, wie durch eine gute Strukturierung von Tabellen und Beziehungen Daten nicht nur korrekt gespeichert, sondern auch effizient abgefragt und verarbeitet werden können.</p>
+  <p>Für mich ist das ein zentrales Thema, weil eine saubere Datenbank der Grundstein für jede stabile und performante Anwendung ist – und ich dieses Wissen gezielt aufbauen möchte, um Projekte langfristig erfolgreich umzusetzen.</p>
 </section>
